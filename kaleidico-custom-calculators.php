4abbeb79ea4bd42fced5e1fc43eae06c2c5ef363
angelo-marasa <angelo.marasa@kaleidico.com>
2024-08-13 10:03:44 -0400

    Update kaleidico-custom-calculators.php

--- kaleidico-custom-calculators.php
+++ kaleidico-custom-calculators.php
@@ -2,7 +2,7 @@
 /*
 Plugin Name: Kaleidico Custom Calculators
 Description: This is a plugin containing mortgage calculators for Kaleidico
-Version: 2.3
+Version: 2.2.1
 Author: Angelo Marasa
 Author URI: https://github.com/angelo-marasa
 */
@@ -23,25 +23,40 @@ $myUpdateChecker = PucFactory::buildUpdateChecker(
     'kaleidico-custom-calculators'
 );
 
-//Set the branch that contains the stable release.
-//$myUpdateChecker->setBranch('stable-branch-name');
+// Set the branch that contains the stable release.
+// $myUpdateChecker->setBranch('stable-branch-name');
 
-//Optional: If you're using a private repository, specify the access token like this:
+// Optional: If you're using a private repository, specify the access token like this:
 // $myUpdateChecker->setAuthentication('your-token-here');
 
 /* -------------------------------------------------------------------------------------- */
 
-
 function kaleidico_custom_calculators_enqueue_scripts()
 {
+    global $post;
+
+    // Enqueue the styles, which might be needed across all pages
     wp_enqueue_style('kaleidico-custom-calculators-style', plugin_dir_url(__FILE__) . 'src/css/kaleidico-custom-calculators.css');
-    wp_enqueue_script('kaleidico-custom-calculators-fha-script', plugin_dir_url(__FILE__) . 'src/js/kaleidico-custom-calculators-fha.js', array('jquery'), '', true);
-    wp_enqueue_script('kaleidico-custom-calculators-affordability-script', plugin_dir_url(__FILE__) . 'src/js/kaleidico-custom-calculators-affordability.js', array('jquery'), '', true);
-    wp_enqueue_script('kaleidico-custom-calculators-mortgage-payment-script', plugin_dir_url(__FILE__) . 'src/js/kaleidico-custom-calculators-mortgage-payment.js', array('jquery'), '', true);
-    wp_enqueue_script('kaleidico-custom-calculators-ui-script', plugin_dir_url(__FILE__) . 'src/js/kaleidico-custom-calculators-ui.js', array('jquery'), '', true);
+
+    // Conditionally enqueue scripts based on the presence of shortcodes
+    if (has_shortcode($post->post_content, 'fha_calculator')) {
+        wp_enqueue_script('kaleidico-custom-calculators-fha-script', plugin_dir_url(__FILE__) . 'src/js/kaleidico-custom-calculators-fha.js', array('jquery'), '', true);
+    }
+
+    if (has_shortcode($post->post_content, 'mortgage-payment-calculator')) {
+        wp_enqueue_script('kaleidico-custom-calculators-mortgage-payment-script', plugin_dir_url(__FILE__) . 'src/js/kaleidico-custom-calculators-mortgage-payment.js', array('jquery'), '', true);
+    }
+
+    if (has_shortcode($post->post_content, 'affordability_calculator')) {
+        wp_enqueue_script('kaleidico-custom-calculators-affordability-script', plugin_dir_url(__FILE__) . 'src/js/kaleidico-custom-calculators-affordability.js', array('jquery'), '', true);
+    }
+
+    // Enqueue UI scripts which might be needed across all calculator pages
+    if (has_shortcode($post->post_content, 'fha_calculator') || has_shortcode($post->post_content, 'mortgage-payment-calculator') || has_shortcode($post->post_content, 'affordability_calculator')) {
+        wp_enqueue_script('kaleidico-custom-calculators-ui-script', plugin_dir_url(__FILE__) . 'src/js/kaleidico-custom-calculators-ui.js', array('jquery'), '', true);
+    }
 }
 add_action('wp_enqueue_scripts', 'kaleidico_custom_calculators_enqueue_scripts');
-
 
 function add_plugin_page_settings_link($links)
 {
