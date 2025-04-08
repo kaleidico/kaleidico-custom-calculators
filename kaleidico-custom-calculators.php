37f90cf3d3f54d0c47b67131f4192d896a2589e0
Angelo Marasa <angelo.marasa@kaleidico.com>
2025-04-08 14:06:03 -0400

    Updated version

--- kaleidico-custom-calculators.php
+++ kaleidico-custom-calculators.php
@@ -2,7 +2,7 @@
 /*
 Plugin Name: Kaleidico Custom Calculators
 Description: This is a plugin containing mortgage calculators for Kaleidico
-Version: 2.5.3
+Version: 2.5.4
 Author: Angelo Marasa
 Author URI: https://github.com/angelo-marasa
 */
